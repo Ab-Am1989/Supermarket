455c10239cdf9e791a7c42d7c220d7d3a8a3f183
amini <abolfazl.amini1990@gmail.com>
2021-08-23 21:33:21 +0430

    login_view path has added to urls.py of account app

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('customer/list/', views.customer_list, name='customer_list'),
     path('customer/<int:customer_id>/', views.customer_details, name='customer_details'),
     path('customer/<int:customer_id>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/login/', views.login_view, name='login_view')
 ]
