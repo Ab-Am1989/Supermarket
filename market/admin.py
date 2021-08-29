1015d438e47b011de3e3c4e9a00d374212ccba11
amini <abolfazl.amini1990@gmail.com>
2021-08-30 01:02:41 +0430

    four models from models.py of market app have registered in admin.py of market app

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from market.models import Order, OrderRow, Customer, Product
+
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(OrderRow)
+admin.site.register(Product)
